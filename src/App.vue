80ef9144544e1fb6296af859dfdb8f200b68b94c
Maximilian Schulze <maximilian.schulze@student.hpi.uni-potsdam.de>
2021-06-06 09:16:12 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main-layout />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MainLayout from './layouts/MainLayout.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { MainLayout }
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn label="Start now" @click="$router.push({ name: 'Login' })" size="xl" />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+});
+</script>
